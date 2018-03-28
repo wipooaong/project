ff976dc55f9047a3d99e58e24f90d5aa086bb54a
Natthapon Sricort <Natthaponsricort@gmail.com>
2018-03-28 13:41:52 +0700

    Fix card

--- resources/views/tips.blade.php
+++ resources/views/tips.blade.php
@@ -4,14 +4,13 @@
 <h1 class="my-4">คำถามที่พบบ่อย</h1>
 @foreach ($knowledge as $k)
     
-@endforeach
 <div class="ui cards w-full">
   <div class="card" style="width:100%;">
     <div class="content">
       <em>{!!  Markdown::convertToHtml($k->text) !!}</em>
     </div>
-  </div>
-
-  
+  </div>  
 </div>
+@endforeach
+
 @endsection
